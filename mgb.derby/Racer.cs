317bd5a2c5959f1b089d268dfbdaeca083cb4fd4
Mike Bingham <binghammg@gmail.com>
2014-02-09 18:10:26 -0700

    Derby initial commit

---
+++ mgb.derby/Racer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mgb.derby
+{
+    class Racer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Racer(string firstname, string lastname)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+        }        
+    }
+}
